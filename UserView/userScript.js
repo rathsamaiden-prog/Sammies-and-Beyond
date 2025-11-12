b46cbdce75d0c852900db008abaa2d0300fb5e8c
Super-Prime1 <superprime1@icloud.com>
2025-11-12 07:47:12 -0500

    getting up to date

--- UserView/userScript.js
+++ UserView/userScript.js
@@ -50,8 +50,8 @@ const apple = new MenuItem(
     "Apple",
     "https://cdn.discordapp.com/attachments/1416088170876895303/1435976344923275306/apple.jpg?ex=690decaf&is=690c9b2f&hm=250721ec6e09846fd33b180a463686037f0291a526df17f7d697ea8df0dbfd61&",
     "This is a fugi apple, it's sweet with a bit of tang.",
-    "Fruit",
-    "1.99"
+    "Vegan",
+    "1.49"
 );
 
 const sammy1 = new MenuItem(
@@ -59,7 +59,7 @@ const sammy1 = new MenuItem(
     "Lunar-loaf.png",
     "Meatball sub with moon dusted parmesan.",
     "Gluten, Dairy, Meat",
-    "14.99"
+    "11.99"
 );
 
 const sammy2 = new MenuItem(
@@ -67,7 +67,7 @@ const sammy2 = new MenuItem(
     "The-Eclipse.png",
     "Black rye bread with white cheddar and ham.",
     "Gluten, Dairy, Meat",
-    "14.99"
+    "10.99"
 );
 
 const sammy3 = new MenuItem(
@@ -75,21 +75,21 @@ const sammy3 = new MenuItem(
     "Rocket-Reuben.png",
     "Toasted multigrain with veggies and crispy onions.",
     "Gluten",
-    "14.99"
+    "9.99"
 );
 const sammy4 = new MenuItem(
     "Cosmic Crunch",
     "Cosmic-Crunch.png",
     "Corned beef, sauerkraut, and cosmic sauce.",
     "Gluten, Meat",
-    "14.99"
+    "10.99"
 );
 const sammy5 = new MenuItem(
     "The Nebula Melt",
     "The-Nebula-Melt.png",
     "swirling layers of cheese and roast beef.",
     "Gluten, Dairy, Meat",
-    "14.99"
+    "10.99"
 );
 
 const sammy6 = new MenuItem(
@@ -97,7 +97,7 @@ const sammy6 = new MenuItem(
     "The-Space-Jamwich.png",
     "peanut butter, banana, and berry jam fusion.",
     "Peanuts, Gluten",
-    "14.99"
+    "8.99"
 );
 
 const sammy7 = new MenuItem(
@@ -105,7 +105,7 @@ const sammy7 = new MenuItem(
     "AstroBLT.png",
     "Classic bacon, lettuce, and tomato with meteorite-spiced mayo.",
     "Gluten, Egg, Meat",
-    "14.99"
+    "9.99"
 );
 
 const sammy8 = new MenuItem(
@@ -113,7 +113,7 @@ const sammy8 = new MenuItem(
     "Solar-Flare-Panini.png",
     "Spicy grilled chicken panini with pepper jack and jalapeños.",
     "Dairy, Gluten, Meat",
-    "13.99"
+    "10.49"
 );
 
 const sammy9 = new MenuItem(
@@ -121,15 +121,15 @@ const sammy9 = new MenuItem(
     "Alien-Avocado-Smash.png",
     "Mashed avocado, lime, garlic, and sprouts on toasted rye.",
     "Gluten",
-    "12.99"
+    "8.99"
 );
 
 const sammy10 = new MenuItem(
     "Galaxy Grilled Cheese",
     "Galaxy-Grilled-Cheese.png",
     "Rainbow cheese blend melted between star-shaped brioche slices.",
-    "Dairy, Gluten",
-    "10.99"
+    "Gluten, Dairy",
+    "7.99"
 );
 
 const sammy11 = new MenuItem(
@@ -137,15 +137,15 @@ const sammy11 = new MenuItem(
     "Black-Hole-BBQ.png",
     "Pulled pork with smoky barbecue sauce on a dark brioche bun.",
     "Gluten, Meat",
-    "14.99"
+    "10.99"
 );
 
 const drink1 = new MenuItem(
     "Comet Cola",
     "Comet-Cola.png",
     "Classic cola with a twist of lime and starfruit.",
-    "tbd",
-    "14.99"
+    "Vegan",
+    "2.99"
 );
 
 const drink2 = new MenuItem(
@@ -153,15 +153,15 @@ const drink2 = new MenuItem(
     "Milky-Way-Shake.png",
     "Chocolate malt with whipped cream and caramel drizzle.",
     "Dairy",
-    "14.99"
+    "4.99"
 );
 
 const drink3 = new MenuItem(
     "AstroFizz",
     "AstroFizz.png",
     "Orange soda with popping candy rim.",
-    "Dairy",
-    "14.99"
+    "Vegan",
+    "3.49"
 );
 
 const drink4 = new MenuItem(
@@ -169,39 +169,39 @@ const drink4 = new MenuItem(
     "Cosmo-Cocoa.png",
     "Chilled chocolate drink with marshmallow meteors.",
     "Dairy",
-    "14.99"
+    "4.49"
 );
 
 const side1 = new MenuItem(
     "Asteroid Fries",
     "Asteroid-Fries.png",
     "Crispy potato wedges with cratered seasoning.",
-    "Dairy",
-    "14.99"
+    "Vegan",
+    "3.99"
 );
 
 const side2 = new MenuItem(
     "Rocket Rings",
     "Rocket-Rings.png",
     "Golden onion rings with spicy launch sauce.",
-    "Dairy",
-    "14.99"
+    "Gluten, Vegetarian",
+    "4.49"
 );
 
 const side3 = new MenuItem(
     "Solar Slaw",
     "Solar-Slaw.png",
     "Tangy cabbage slaw with sunburst citrus dressing.",
-    "Dairy",
-    "14.99"
+    "Vegan",
+    "3.49"
 );
 
 const side4 = new MenuItem(
     "Orbit Tots",
     "Orbit-Tots.png",
     "Seasoned tater tots with planetary dipping sauces.",
-    "Dairy",
-    "14.99"
+    "Vegetarian",
+    "4.49"
 );
 
 menuRow1.insertAdjacentHTML("beforeend", sammy1.appendItem());
