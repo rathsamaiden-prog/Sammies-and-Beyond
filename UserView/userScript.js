f11410e497522c066f6e25a4864157ab598ae7ad
joshua <joshcuebas333@aol.com>
2025-11-24 10:04:27 -0500

    minor changes

--- UserView/userScript.js
+++ UserView/userScript.js
@@ -307,6 +307,7 @@ document.addEventListener("DOMContentLoaded", function () {
         checkoutBtn.style.display = "none";
         paymentScreen.style.display = "block";
     });
+})
 
     const deliveryForm = document.getElementById("fake-delivery-form");
     const orderRadios = document.getElementsByName("order");
