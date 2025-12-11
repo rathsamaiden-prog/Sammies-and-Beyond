6d3926e3a40dc2a8d43cd8691c457b77af38faca
rathsamaiden-prog <rathsamaiden@gmail.com>
2025-12-11 07:34:47 -0500

    Update navigation URLs to absolute paths

--- homeScreen/home.js
+++ homeScreen/home.js
@@ -1,16 +1,16 @@
 document.getElementById("sammies").addEventListener("click", () => {
-    window.location.href = "./userView/userView.html";
+    window.location.href = "/Sammies-and-Beyond/userView/userView.html";
 });
 
 document.getElementById("sides").addEventListener("click", () => {
-    window.location.href = "./userView/userView.html#sides";
+    window.location.href = "/Sammies-and-Beyond/userView/userView.html#sides";
 });
 
 document.getElementById("drinks").addEventListener("click", () => {
-    window.location.href = "./userView/userView.html#drinks";
+    window.location.href = "/Sammies-and-Beyond/userView/userView.html#drinks";
 });
 document.getElementById("order").addEventListener("click", () => {
-    window.location.href = "./userView/userView.html";
+    window.location.href = "/Sammies-and-Beyond/userView/userView.html";
 });
 
 
