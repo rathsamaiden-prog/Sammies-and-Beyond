8d7871af380a289efdfea4729e4d20de13f4afb2
rathsamaiden-prog <rathsamaiden@gmail.com>
2025-12-11 07:30:22 -0500

    Update navigation paths to use relative URLs

--- homeScreen/home.js
+++ homeScreen/home.js
@@ -1,16 +1,16 @@
 document.getElementById("sammies").addEventListener("click", () => {
-    window.location.href = "../userView/userView.html";
+    window.location.href = "./userView/userView.html";
 });
 
 document.getElementById("sides").addEventListener("click", () => {
-    window.location.href = "../userView/userView.html#sides";
+    window.location.href = "./userView/userView.html#sides";
 });
 
 document.getElementById("drinks").addEventListener("click", () => {
-    window.location.href = "../userView/userView.html#drinks";
+    window.location.href = "./userView/userView.html#drinks";
 });
 document.getElementById("order").addEventListener("click", () => {
-    window.location.href = "../userView/userView.html";
+    window.location.href = "./userView/userView.html";
 });
 
 
